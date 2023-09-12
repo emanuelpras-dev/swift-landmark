19057c38aeb7041c1067a6d8e76b22694e7084ce
emanuelprass <emanuelpras4@gmail.com>
2023-09-12 10:43:41 +0700

    Initial Commit

---
+++ landmark/landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkApp.swift
+//  landmark
+//
+//  Created by Emanuel Prasetyawan on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
